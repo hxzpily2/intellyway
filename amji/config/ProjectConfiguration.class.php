89e81c9d0c0b37202352cf872219e7ffddaca14f
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-01-04 15:38:51 +0000

    init

--- amji/config/ProjectConfiguration.class.php
+++ amji/config/ProjectConfiguration.class.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'C:\\wamp\\www\\symfony\\pear\\symfony/autoload/sfCoreAutoload.class.php';
+//require_once '..\lib\symfony\autoload\sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
