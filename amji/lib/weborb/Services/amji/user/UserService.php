39a815a792eba689aa0edd36c4011c1dc4bbe65e
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-01-07 15:12:12 +0000

    init

--- amji/lib/weborb/Services/amji/user/UserService.php
+++ amji/lib/weborb/Services/amji/user/UserService.php
@@ -5,14 +5,10 @@ require_once dirname(__FILE__).'/../generic/GenericService.php';
 class UserService extends GenericService{
 	protected $context = null;
 
-	public function createUser(){
-		//$user = new User();
-
-			
-
-		$user = new User();
-
-		return "test";
+	public function createUser(CreateUserVO $user){
+		$user->email = "test";
+		$user->password = "000000";
+		return AmjiUser::createUser($user);
 	}
 }
 
