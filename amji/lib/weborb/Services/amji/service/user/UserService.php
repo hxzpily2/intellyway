e40f15812c2dded9040d1d43a5bc428c4fd9b598
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-01-27 09:46:35 +0000

    maj

--- amji/lib/weborb/Services/amji/service/user/UserService.php
+++ amji/lib/weborb/Services/amji/service/user/UserService.php
@@ -17,6 +17,22 @@ class UserService extends GenericService{
 		$user = $this->getUser()->getAttribute(Sessions::USERCONNECTED);
 		return AmjiUser::inviteContact($user->getIduser(),$idcontact);
 	}
+	
+	public function login($login,$password){
+		$user = AmjiUser::getUser ( $login, $password );
+		if($user!=NULL){
+			$this->getUser ()->setAttribute ( Sessions::USERCONNECTED, $user );
+			$this->getUser ()->setAttribute ( 'user_id', $user->getId (), 'sfGuardSecurityUser' );
+			$this->getUser ()->setAuthenticated ( true );
+			$this->getUser ()->clearCredentials ();
+			$this->getUser ()->addCredentials ( $user->getAllPermissionNames () );
+			$user->setLastLogin ( date ( 'Y-m-d h:i:s' ) );
+			return $user;
+		}else{
+			return NULL;
+		}
+		$user->save ();		
+	}
 }
 
 ?>
