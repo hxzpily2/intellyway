b33fe983f2939b94d65d59b95f5f6a703a4ec1ee
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-06-24 16:31:06 +0000

    INIT

--- PDFHandler/src/com/bedreamy/base/PDFHighlighting.java
+++ PDFHandler/src/com/bedreamy/base/PDFHighlighting.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import org.apache.lucene.document.Document;
 import org.apache.pdfbox.examples.util.PrintTextLocations;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.searchengine.lucene.LucenePDFDocument;
 
 
@@ -15,6 +16,7 @@ import org.apache.pdfbox.searchengine.lucene.LucenePDFDocument;
 public class PDFHighlighting {
 	
 	private static final String LASTPAGE = "-lastPage";    
+	private static final String OCCURENCE = "-occurence";
     
 	public static void main( String[] args ) throws IOException{
 		//Document luceneDocument = LucenePDFDocument.getDocument(new File("test.pdf"));
@@ -23,7 +25,16 @@ public class PDFHighlighting {
 		 ArrayList allPages = (ArrayList) doc.getDocumentCatalog().getAllPages();
 		 System.out.println(allPages.size());
 
-		 
+		for (int i = 0; i < allPages.size(); i++) {
+			PDPage page = (PDPage) allPages.get(i);
+			System.out.println("Processing page: " + i);
+			printer.processStream(page, page.findResources(), page.getContents().getStream());
+			
+		}
+		
+		 doc.close();
+			 
+
 
 	}
 	
