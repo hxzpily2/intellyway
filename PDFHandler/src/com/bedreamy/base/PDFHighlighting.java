607796b432301bd61e321c088cbf810595d10276
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-06-24 16:14:33 +0000

    INIT

--- PDFHandler/src/com/bedreamy/base/PDFHighlighting.java
+++ PDFHandler/src/com/bedreamy/base/PDFHighlighting.java
@@ -1,9 +1,13 @@
 package com.bedreamy.base;
 
+import java.awt.List;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.apache.lucene.document.Document;
+import org.apache.pdfbox.examples.util.PrintTextLocations;
+import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.searchengine.lucene.LucenePDFDocument;
 
 
@@ -14,6 +18,12 @@ public class PDFHighlighting {
     
 	public static void main( String[] args ) throws IOException{
 		//Document luceneDocument = LucenePDFDocument.getDocument(new File("test.pdf"));
+		 PDDocument doc = PDDocument.load(new File("test.pdf"));
+		 PrintTextLocations printer = new PrintTextLocations();
+		 ArrayList allPages = (ArrayList) doc.getDocumentCatalog().getAllPages();
+		 System.out.println(allPages.size());
+
+		 
 
 	}
 	
