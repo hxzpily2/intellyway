d3cb8e649b63cdaa65be647607c645c2607d4d6d
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-06-29 12:27:06 +0000

    INIT

--- PDFViewer/bin/win32/ajaxgeneratepage.bat
+++
@@ -1,2 +0,0 @@
-set CLASSPTH=../jar/pdfhandler.jar;../jar/commons-logging.jar;../jar/commons-logging-1.0.4.jar;../jar/fontbox-1.1.0.jar;../jar/jempbox-1.1.0.jar;../jar/json_simple-1.1.jar;../jar/log4j-1.2.15.jar;../jar/lucene-core-3.0.2.jar;../jar/pdfbox-1.1.0.jar
-java -classpath %CLASSPTH%  com.bedreamy.base.PDFGetBookmarks
