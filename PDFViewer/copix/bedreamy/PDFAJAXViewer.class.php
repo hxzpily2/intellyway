53bb83f536e3e3e0d7056210b1fceae1e9692cd7
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-06-29 16:59:45 +0000

    INIT

--- PDFViewer/copix/bedreamy/PDFAJAXViewer.class.php
+++ PDFViewer/copix/bedreamy/PDFAJAXViewer.class.php
@@ -1,5 +1,19 @@
 <?php
 class PDFAJAXViewer{
+	
+	const FILENOTFOUND = "FILENOTFOUND";
+	const BOOKMARKFILE = "bookmark.json";
+	const INFORMATIONFILE = "info";
+	
+	static $DOCUMENT = "";
+	static $INFOURL = "";
+	static $BOOKMARKURL = "";
+	static $INFOBASEURLJSON = "";
+	static $INFOBASEURLXML = "";
+	static $NBPAGES = "";
+	static $CURRENTPAGE = "";
+	static $DEFAULT_RESOL = "100";
+	
 	public static function showViewer(){
 		return "test";
 	}
@@ -28,6 +42,77 @@ class PDFAJAXViewer{
    			 " dojo.require('bedreamy.integration.Commun'); ".
    			 " var commun = new bedreamy.integration.Commun(); ".   
 			 " </script> ";
+	}
+	
+	public static function getBookmarkJSON($pdfURL){
+		PDFAJAXViewer::$DOCUMENT = $pdfURL;
+		$classpath  = ResourceBundle::get('pdfviewer.absolute.path.base')."/bin/jar/pdfhandler.jar;";
+		$classpath .= ResourceBundle::get('pdfviewer.absolute.path.base')."/bin/jar/commons-logging.jar;";
+		$classpath .= ResourceBundle::get('pdfviewer.absolute.path.base')."/bin/jar/commons-logging-1.0.4.jar;";
+		$classpath .= ResourceBundle::get('pdfviewer.absolute.path.base')."/bin/jar/fontbox-1.1.0.jar;";
+		$classpath .= ResourceBundle::get('pdfviewer.absolute.path.base')."/bin/jar/jempbox-1.1.0.jar;";
+		$classpath .= ResourceBundle::get('pdfviewer.absolute.path.base')."/bin/jar/json_simple-1.1.jar;";
+		$classpath .= ResourceBundle::get('pdfviewer.absolute.path.base')."/bin/jar/log4j-1.2.15.jar;";
+		$classpath .= ResourceBundle::get('pdfviewer.absolute.path.base')."/bin/jar/lucene-core-3.0.2.jar;";
+		$classpath .= ResourceBundle::get('pdfviewer.absolute.path.base')."/bin/jar/pdfbox-1.1.0.jar";
+		
+		if(file_exists($pdfURL)){
+			$cacheDirectory = ResourceBundle::get('pdfviewer.absolute.path.base')."/cache/".PDFAJAXViewer::getForlderName($pdfURL);			
+			$jsonFile = $cacheDirectory."/".PDFAJAXViewer::BOOKMARKFILE;
+			$jsonInfoFile = $cacheDirectory."/".PDFAJAXViewer::INFORMATIONFILE;
+			PDFAJAXViewer::$INFOURL = ResourceBundle::get('pdfviewer.relatif.path.base')."/cache/".PDFAJAXViewer::getForlderName($pdfURL)."/".PDFAJAXViewer::INFORMATIONFILE.".json";
+			PDFAJAXViewer::$BOOKMARKURL = ResourceBundle::get('pdfviewer.relatif.path.base')."/cache/".PDFAJAXViewer::getForlderName($pdfURL)."/".PDFAJAXViewer::BOOKMARKFILE;
+			PDFAJAXViewer::$INFOBASEURLJSON = ResourceBundle::get('pdfviewer.absolute.path.base')."/cache/".PDFAJAXViewer::getForlderName($pdfURL)."/".PDFAJAXViewer::INFORMATIONFILE.".json"; 
+			PDFAJAXViewer::$INFOBASEURLXML = ResourceBundle::get('pdfviewer.absolute.path.base')."/cache/".PDFAJAXViewer::getForlderName($pdfURL)."/".PDFAJAXViewer::INFORMATIONFILE.".xml";
+			if(file_exists($cacheDirectory)){
+				if(!file_exists($jsonFile) || !file_exists($jsonInfoFile)){
+					exec("java -classpath $classpath com.bedreamy.base.PDFGetBookmarks $pdfURL -destination $jsonFile -destinationInfo $jsonInfoFile");
+				}				
+			}else{								
+				mkdir($cacheDirectory,0777);
+				exec("java -classpath $classpath com.bedreamy.base.PDFGetBookmarks $pdfURL -destination $jsonFile -destinationInfo $jsonInfoFile");				 	
+			}
+			PDFAJAXViewer::getNbPages();
+			PDFAJAXViewer::generatePage(1,PDFAJAXViewer::$DEFAULT_RESOL);			
+		}else{
+			throw new Exception(FILENOTFOUND);
+		}
+						 
+	}
+	
+	public static function generatePage($page,$resolution){
+		$pagerel = ResourceBundle::get('pdfviewer.relatif.path.base')."/cache/".sha1(PDFAJAXViewer::$DOCUMENT);
+		$pageabs = ResourceBundle::get('pdfviewer.absolute.path.base')."/cache/".sha1(PFAJAXViewer::$DOCUMENT);
+		PDFAJAXViewer::$CURRENTPAGE = $pagerel;
+		echo "java -classpath $classpath com.bedreamy.base.PDFToImage ".PDFAJAXViewer::$DOCUMENT." -imageType png  -startPage $page -endPage $page -resolution $resolution -outputPrefix $pageabs";
+		if(!file_exists($pageabs)){
+			exec("java -classpath $classpath com.bedreamy.base.PDFToImage ".PDFAJAXViewer::$DOCUMENT." -imageType png  -startPage $page -endPage $page -resolution $resolution -outputPrefix $pageabs");
+		}
+		return PDFAJAXViewer::$CURRENTPAGE;
+	}
+	
+	public static function getPDFNameFromURL($pdfURL){
+		return basename($pdfURL);
+	}
+	
+	public static function getForlderName($pdfURL){
+		return sha1($pdfURL);
+	}
+	
+	public static function execIsEnabled(){
+		$disabled = explode(', ', ini_get('disable_functions'));
+  		return !in_array('exec', $disabled);
+	}
+	
+	public static function getNbPages(){
+		
+		$xml = simplexml_load_file(PDFAJAXViewer::$INFOBASEURLXML);
+		foreach($xml as $nom=>$elem) {
+			if($nom="page"){
+				PDFAJAXViewer::$NBPAGES = $elem;
+				break;
+			}
+		}		
 	}
 }
 ?>
