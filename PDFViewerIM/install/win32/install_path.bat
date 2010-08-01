68e3748d30a2f25ee314ddfd354344eba2b6e1b4
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-08-01 20:10:51 +0000

    INIT

--- PDFViewerIM/install/win32/install_path.bat
+++ PDFViewerIM/install/win32/install_path.bat
@@ -1,7 +1,1 @@
-reg Query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v path | findstr /i /c:"C:\pdfviewerbin\gs8.71\bin" >nul
- if not %errorlevel%== 0 REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /d "%PATH%;C:\pdfviewerbin\gs8.71\bin" /f
-
-reg Query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v path | findstr /i /c:"C:\pdfviewerbin\imagemagick-6.6.3.1" >nul
- if not %errorlevel%== 0 REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /d "%PATH%;C:\pdfviewerbin\imagemagick-6.6.3.1" /f
- 
- 
+REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /d "%PATH%;C:\pdfviewerbin\gs8.71\bin;C:\pdfviewerbin\imagemagick-6.6.3.1" /f
