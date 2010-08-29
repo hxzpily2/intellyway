ad5ffa094f1d33f6e00bbfc1389757efa9fe4c2c
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-08-29 17:26:55 +0000

    MAJ

---
+++ comoresBANK/docs/js/account/integration/Commun.js
@@ -0,0 +1,48 @@
+
+dojo.provide("account.integration.Commun");
+
+dojo.declare("account.integration.Commun",[],{
+
+	getPage : function (url,page,resolution,assets){
+    		dojo.style("loader", "opacity", 0.7);
+	    	var loader = dojo.byId('loader');
+	    	loader.style.display = "";
+            dojo.xhrPost ({
+                url: url+"?request=getpage&page="+page+"&resolution="+resolution,
+                handleAs: "text",
+                preventCache:true,
+                load: dojo.hitch(this,function (response) {
+                	dojo.byId('page').innerHTML = response;
+                	dojo.byId('numPage').value=page;
+                	$("#page").scrollview({
+        			    grab:assets+"/js/scroller/images/openhand.cur",
+        			    grabbing:assets+"/js/scroller/images/closedhand.cur"
+        			});
+                	
+                	dojo.fadeOut({ node: loader, duration:500,
+        				onEnd: function(){
+        					loader.style.display = "none";
+        				}
+        			}).play();
+                }
+                ),
+                error: function (data) {
+                    console.error('Error: chargement services', data);
+                }
+            });        
+		
+    }, 
+    
+    writePWD : function (number){
+    	if(dojo.byId('password').value.length<5){
+    		dojo.byId('j_password').value=dojo.byId('j_password').value+String.fromCharCode(number+64);
+    		dojo.byId('password').value=dojo.byId('password').value+String.fromCharCode(number+64);
+    	}
+    },
+    
+    corPWD : function (){
+    	dojo.byId('j_password').value= "";
+    	dojo.byId('password').value= "";
+    }
+    
+});
