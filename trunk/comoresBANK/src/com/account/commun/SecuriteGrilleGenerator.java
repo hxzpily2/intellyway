b5c07dfaff868084a6d0a3e19e3f399106679525
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-08-29 17:28:58 +0000

    MAJ

--- trunk/comoresBANK/src/com/account/commun/SecuriteGrilleGenerator.java
+++ trunk/comoresBANK/src/com/account/commun/SecuriteGrilleGenerator.java
@@ -8,6 +8,9 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Vector;
 
 import javax.imageio.ImageIO;
@@ -39,12 +42,14 @@ public class SecuriteGrilleGenerator {
 			items.remove(randomI);
 			tableau[itemI] = itemK;
 			// System.out.println(randomK+" "+randomI);
-		}		
+		}
 		return tableau;
 	}
-	
-	public static String updateImageWithGrilleToLogin(int[] tableau,String imageURLIN,String imageURLOUT,long timestamp) throws IOException {
-		
+
+	public static String updateImageWithGrilleToLogin(int[] tableau,
+			String imageURLIN, String imageURLOUT, long timestamp)
+			throws IOException {
+
 		File file = new File(imageURLIN);
 		Image image = ImageIO.read(file);
 
@@ -54,23 +59,24 @@ public class SecuriteGrilleGenerator {
 
 		Font font = new Font("Tahoma", Font.BOLD, 16);
 		ig2.setFont(font);
-		
-		int PLUS = 26;		
-		
+
+		int PLUS = 26;
+
 		ig2.setPaint(Color.WHITE);
 		int Ytemp = 0;
-		for(int i=0;i<5;i++){
-			int Xtemp = 0;			
-			for(int j=0;j<5;j++){
-				if(tableau[i*5+j]!=0)
-					ig2.drawString(new Integer(tableau[i*5+j]).toString(), 8+Xtemp, 18+Ytemp);
-				Xtemp+=PLUS;
+		for (int i = 0; i < 5; i++) {
+			int Xtemp = 0;
+			for (int j = 0; j < 5; j++) {
+				if (tableau[i * 5 + j] != 0)
+					ig2.drawString(new Integer(tableau[i * 5 + j]).toString(),
+							8 + Xtemp, 18 + Ytemp);
+				Xtemp += PLUS;
 			}
-			Ytemp+=PLUS;
-		}		
-		
-		ImageIO.write(bi, "PNG", new File(imageURLOUT+timestamp+".PNG"));
-		return "c:\\"+timestamp+".PNG";
+			Ytemp += PLUS;
+		}
+
+		ImageIO.write(bi, "PNG", new File(imageURLOUT + timestamp + ".PNG"));
+		return "c:\\" + timestamp + ".PNG";
 	}
 
 	public static BufferedImage bufferImage(Image image, int type) {
@@ -81,4 +87,32 @@ public class SecuriteGrilleGenerator {
 		// waitForImage(bufferedImage);
 		return bufferedImage;
 	}
+	
+	private static String convertToHex(byte[] data) { 
+        StringBuffer buf = new StringBuffer();
+        for (int i = 0; i < data.length; i++) { 
+            int halfbyte = (data[i] >>> 4) & 0x0F;
+            int two_halfs = 0;
+            do { 
+                if ((0 <= halfbyte) && (halfbyte <= 9)) 
+                    buf.append((char) ('0' + halfbyte));
+                else 
+                    buf.append((char) ('a' + (halfbyte - 10)));
+                halfbyte = data[i] & 0x0F;
+            } while(two_halfs++ < 1);
+        } 
+        return buf.toString();
+    } 
+
+
+	public static String SHA1(String text) throws NoSuchAlgorithmException,
+			UnsupportedEncodingException {
+		MessageDigest md;
+		md = MessageDigest.getInstance("SHA-1");
+		byte[] sha1hash = new byte[40];
+		md.update(text.getBytes("iso-8859-1"), 0, text.length());
+		sha1hash = md.digest();
+		return convertToHex(sha1hash);
+	}
+
 }
