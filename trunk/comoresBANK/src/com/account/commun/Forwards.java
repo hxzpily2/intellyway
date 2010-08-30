73570c5dfb1da8f501d8ba0c804e5effe07f0dba
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-08-30 09:02:26 +0000

    INIT

--- trunk/comoresBANK/src/com/account/commun/Forwards.java
+++ trunk/comoresBANK/src/com/account/commun/Forwards.java
@@ -3,4 +3,5 @@ package com.account.commun;
 public class Forwards {
 	public static String LOGIN = "login";
 	public static String SHOWLOGINPAGE = "loginPage";
+	public static String SHOWHOMEPAGE = "homePage";
 }
