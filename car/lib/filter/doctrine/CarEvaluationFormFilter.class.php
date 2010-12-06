bf91e52012896aaba4148ad44b7b8b2443189ebb
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-12-06 11:03:34 +0000

    INIT

---
+++ car/lib/filter/doctrine/CarEvaluationFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * CarEvaluation filter form.
+ *
+ * @package    car
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class CarEvaluationFormFilter extends BaseCarEvaluationFormFilter
+{
+  public function configure()
+  {
+  }
+}
