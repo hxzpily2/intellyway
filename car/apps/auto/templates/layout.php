1727dd6a21f4c1d3072a0a3c2bc9d3b47c973b9c
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-12-14 17:25:33 +0000

    INIT

--- car/apps/auto/templates/layout.php
+++ car/apps/auto/templates/layout.php
@@ -76,7 +76,19 @@ dojo.registerModulePath('car', '../../car');
 dojo.require('car.integration.Commun');
 var commun = new car.integration.Commun();
 
+dojo.addOnLoad(
+  function(){	  
+	  /*dojo.connect(dojo.byId('background'), "onmouseover", function(e){
+		  dojo.style(dojo.byId('background'), 'opacity', .5);
+		  dojo.style(dojo.byId('background'), 'zIndex', 999);
+	  });
 
+	  dojo.connect(dojo.byId('background'), "onmouseout", function(e){
+		  dojo.style(dojo.byId('background'), 'opacity', 1.0);
+		  dojo.style(dojo.byId('background'), 'zIndex', 999);
+	  });*/
+  }
+);
 	
 </script>
 <!-- FIN DOJO --> 
@@ -85,73 +97,89 @@ var commun = new car.integration.Commun();
 
 </head>
 <body>
-<!-- <img src="/car/web/images/bg/Bugatti-Galibier_Concept_2009_1600x1200_wallpaper_17.jpg" alt="" id="background" />
-<div id="maincontent">
-  web site here
-</div>-->
-
-<div id="topnav" class="topnav">Have an account? <a href="#"
-	class="signin"><span>Sign in</span></a></div>
-<fieldset id="signin_menu">
-<form method="post" id="signin" action=""><label for="username">Username
-or email</label> <input id="username" name="username" value=""
-	title="username" tabindex="4" type="text">
-</p>
-<p><label for="password">Password</label> <input id="password"
-	name="password" value="" title="password" tabindex="5" type="password"></p>
-<p class="remember"><input id="signin_submit" value="Sign in"
-	tabindex="6" type="submit"> <input id="remember" name="remember_me"
-	value="1" tabindex="7" type="checkbox"> <label for="remember">Remember
-me</label></p>
-<p class="forgot"><a href="#" id="resend_password_link">Forgot your
-password?</a></p>
-<!-- <p class="forgot-username">
-          <A id=forgot_username_link title="If you remember your password, try logging in with your email" href="#">Forgot your username?</A>        </p> --></form>
-</fieldset>
-<div style="clear: both;"></div>
-</div>
-
-<script type="text/javascript">
-        $(document).ready(function() {
-
-            $(".signin").click(function() {
-                $("fieldset#signin_menu").toggle();
-				$(".signin").toggleClass("menu-open");
-            });
-			
-			$("fieldset#signin_menu").mouseup(function() {
-				return false;
-			});
-			$(document).mouseup(function(e) {
-				if($(e.target).parent("a.signin").length==0) {
-					$(".signin").removeClass("menu-open");
-					$("fieldset#signin_menu").hide();
-				}
-			});	
-
-			(function($) {
-			  $("#background").fullBg();
-			})(jQuery);		
-			
-        });
-  </script>
-<script src="/car/web/js/jquery/jquery.tipsy.js" type="text/javascript"></script>
-<script type='text/javascript'>
-    $(function() {
-	  $('#forgot_username_link').tipsy({gravity: 'w'});   
-    });
-  </script>
-
-
-
-<div id="main">	
+<img src="/car/web/images/bg/Bugatti-Galibier_Concept_2009_1600x1200_wallpaper_17.jpg" alt="" id="background" />
+<div id="maincontent">  
+	<div id="topnav" class="topnav">
+	   Have an account? 
+	   <a href="#" class="signin"><span>Sign in</span></a>
+	</div>
+	<fieldset id="signin_menu">
+	<form method="post" id="signin" action="">
+		<label for="username">Username or email</label>
+		<input id="username" name="username" value="" title="username" tabindex="4" type="text">
+	
+	<p>
+		<label for="password">Password</label>
+		<input id="password" name="password" value="" title="password" tabindex="5" type="password">
+	</p>
+	<p class="remember">
+		<input id="signin_submit" value="Sign in" tabindex="6" type="submit">
+		<input id="remember" name="remember_me" value="1" tabindex="7" type="checkbox">
+		<label for="remember">Remember me</label>
+	</p>
+	<p class="forgot">
+		<a href="#" id="resend_password_link">Forgot your password?</a>
+	</p>
+	<!-- <p class="forgot-username">
+	          <A id=forgot_username_link title="If you remember your password, try logging in with your email" href="#">Forgot your username?</A>        </p> --></form>
+	</fieldset>
+	<div style="clear: both;"></div>
+	
+	
+	<script type="text/javascript">
+	        $(document).ready(function() {
+	
+	            $(".signin").click(function() {
+	                $("fieldset#signin_menu").toggle();
+					$(".signin").toggleClass("menu-open");
+	            });
+				
+				$("fieldset#signin_menu").mouseup(function() {
+					return false;
+				});
+				$(document).mouseup(function(e) {
+					if($(e.target).parent("a.signin").length==0) {
+						$(".signin").removeClass("menu-open");
+						$("fieldset#signin_menu").hide();
+					}
+				});	
+	
+				(function($) {
+				  $("#background").fullBg();
+				})(jQuery);		
+				
+	        });
+	  </script>
+	<script src="/car/web/js/jquery/jquery.tipsy.js" type="text/javascript"></script>
+	<script type='text/javascript'>
+	    $(function() {
+		  $('#forgot_username_link').tipsy({gravity: 'w'});   
+	    });
+	  </script>
+	
+	
+	<!-- CONTENT -->
+	<div id="main">
 	<?php echo $sf_content ?>
+	</div>
+	<!-- FIN CONTENT -->	
 </div>
-
-
-
-
-
+<div id="footerOutside">
+	<div id="footer">
+		<ul class="nav">
+			<li><a href="#">contact</a></li>
+			<li><a href="#">forum</a>|</li>
+			<li><a href="#">blog</a>|</li>
+			<li><a href="#">chat</a>|</li>
+			<li><a href="#">support</a>|</li>
+			<li><a href="#">home</a>|</li>
+		</ul>
+	<a href="http://validator.w3.org/check?uri=referer" target="_blank" class="xhtml" title="xhtml">XHTML</a>
+	<a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank" class="css" title="css">CSS</a><br class="spacer" />
+	<p class="copyright">©yellow web. All rights reserved.</p>
+	<p class="design">Designed By: <a href="http://www.templateworld.com" target="_blank" title="template world">Template World</a></p>
+	</div>
+</div>	
 <div id='loader' style="display: none;"><!-- <div id='loaderInner' style='direction: ltr;'>Loading theme Tester ...</div>  -->
 	<table width='100%' height='100%'>
 		<tr>
@@ -160,6 +188,5 @@ password?</a></p>
 		</tr>
 	</table>
 </div>
-
 </body>
 </html>
