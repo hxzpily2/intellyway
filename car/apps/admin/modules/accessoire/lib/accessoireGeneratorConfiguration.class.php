a8c145fa50f8a6e375de3b7f1f544a20ae2a6293
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-12-27 15:34:04 +0000

    INIT

---
+++ car/apps/admin/modules/accessoire/lib/accessoireGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * accessoire module configuration.
+ *
+ * @package    car
+ * @subpackage accessoire
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class accessoireGeneratorConfiguration extends BaseAccessoireGeneratorConfiguration
+{
+}
