e20ff1f7db6b646ec15e0fe06aa7285639e2b0a1
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-12-27 15:51:49 +0000

    INIT

---
+++ car/apps/auto/modules/annonce/lib/annonceGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * annonce module helper.
+ *
+ * @package    car
+ * @subpackage annonce
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class annonceGeneratorHelper extends BaseAnnonceGeneratorHelper
+{
+}
