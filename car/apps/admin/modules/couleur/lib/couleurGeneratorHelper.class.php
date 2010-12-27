09668a558b85c19e15ae947e4a5e890b7e351392
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-12-27 15:35:18 +0000

    INIT

---
+++ car/apps/admin/modules/couleur/lib/couleurGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * couleur module helper.
+ *
+ * @package    car
+ * @subpackage couleur
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class couleurGeneratorHelper extends BaseCouleurGeneratorHelper
+{
+}
