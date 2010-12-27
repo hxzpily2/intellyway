a000ba7fa745c83953a42ff95d19c404a545ed80
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-12-27 15:34:54 +0000

    INIT

---
+++ car/apps/admin/modules/concessio/lib/concessioGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * concessio module configuration.
+ *
+ * @package    car
+ * @subpackage concessio
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class concessioGeneratorConfiguration extends BaseConcessioGeneratorConfiguration
+{
+}
