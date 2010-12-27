00c76c4eb37f529d22b5c336125aaa833b829c77
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-12-27 15:36:09 +0000

    INIT

---
+++ car/apps/admin/modules/expert/lib/expertGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * expert module configuration.
+ *
+ * @package    car
+ * @subpackage expert
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class expertGeneratorConfiguration extends BaseExpertGeneratorConfiguration
+{
+}
---
+++ car/apps/admin/modules/expert/lib/expertGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * expert module helper.
+ *
+ * @package    car
+ * @subpackage expert
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class expertGeneratorHelper extends BaseExpertGeneratorHelper
+{
+}
