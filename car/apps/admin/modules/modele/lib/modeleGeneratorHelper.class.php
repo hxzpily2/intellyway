e71f8762bca1002399101a462fc39348d3dfa963
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-12-27 15:38:40 +0000

    INIT

---
+++ car/apps/admin/modules/modele/lib/modeleGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * modele module helper.
+ *
+ * @package    car
+ * @subpackage modele
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class modeleGeneratorHelper extends BaseModeleGeneratorHelper
+{
+}
