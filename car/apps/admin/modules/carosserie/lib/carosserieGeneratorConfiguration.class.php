a5b84b182d2b7c49c9af88d7a9ffac5acde789f3
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-12-27 15:34:33 +0000

    INIT

---
+++ car/apps/admin/modules/carosserie/lib/carosserieGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * carosserie module configuration.
+ *
+ * @package    car
+ * @subpackage carosserie
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class carosserieGeneratorConfiguration extends BaseCarosserieGeneratorConfiguration
+{
+}
---
+++ car/apps/admin/modules/carosserie/lib/carosserieGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * carosserie module helper.
+ *
+ * @package    car
+ * @subpackage carosserie
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class carosserieGeneratorHelper extends BaseCarosserieGeneratorHelper
+{
+}
