24a13d26fe78786b935483db1f36d5848253c737
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-12-27 15:39:48 +0000

    INIT

---
+++ car/apps/admin/modules/vitesse/lib/vitesseGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * vitesse module helper.
+ *
+ * @package    car
+ * @subpackage vitesse
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class vitesseGeneratorHelper extends BaseVitesseGeneratorHelper
+{
+}
