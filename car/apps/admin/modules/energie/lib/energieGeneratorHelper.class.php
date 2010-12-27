7bca99e5a7fe1e62c47f963a5f50df3ea554f3b4
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2010-12-27 15:35:30 +0000

    INIT

---
+++ car/apps/admin/modules/energie/lib/energieGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * energie module helper.
+ *
+ * @package    car
+ * @subpackage energie
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class energieGeneratorHelper extends BaseEnergieGeneratorHelper
+{
+}
