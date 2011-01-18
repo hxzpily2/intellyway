0540dcb2d143bab27d29469de4a273870a745402
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2011-01-18 15:15:19 +0000

    INIT

---
+++ car/apps/admin/modules/adrloc/lib/adrlocGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * adrloc module configuration.
+ *
+ * @package    car
+ * @subpackage adrloc
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class adrlocGeneratorConfiguration extends BaseAdrlocGeneratorConfiguration
+{
+}
