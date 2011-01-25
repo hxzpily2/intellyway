a70522b5faf904362374c6110c304f80788754d2
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2011-01-25 16:22:55 +0000

    INIT

---
+++ car/web/js/ext/combos/combos.js
@@ -0,0 +1,26 @@
+/*!
+ * Ext JS Library 3.3.1
+ * Copyright(c) 2006-2010 Sencha Inc.
+ * licensing@sencha.com
+ * http://www.sencha.com/license
+ */
+Ext.onReady(function(){
+    Ext.QuickTips.init();
+
+    // simple array store
+    var store = new Ext.data.ArrayStore({
+        fields: ['abbr', 'state', 'nick'],
+        data : Ext.exampledata.states // from states.js
+    });
+    var combo = new Ext.form.ComboBox({
+        store: store,
+        displayField:'state',
+        typeAhead: true,
+        mode: 'local',
+        forceSelection: true,
+        triggerAction: 'all',
+        emptyText:'Select a state...',
+        selectOnFocus:true,
+        applyTo: 'car_auto[idmarque]'
+    });       
+});
