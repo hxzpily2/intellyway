9f6d2fac498cff3c26a0d1926305a9c27d8db7c8
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2011-02-15 17:00:03 +0000

    INIT

--- car/apps/auto/modules/annonce/templates/annonceSuccess.php
+++ car/apps/auto/modules/annonce/templates/annonceSuccess.php
@@ -12,15 +12,40 @@
                 <span style="font-family: TAHOMA;color: #C61B00;font-size: 16pt;padding-left: 10px;"><span style="font-weight: bold;">F&eacute;licitations </span></span>
             </div>
             <br/>
-            <div style="padding-left: 10px;font-family: TAHOMA;font-size: 10pt">
-                <table>
-                    <tr>
-                        <td width="10" valign="top" style="padding-top: 5px;"><img src="/car/web/images/red_fleche.png"/></td>
-                        <td width="3"></td>
-                        <td valign="top">Votre annonce portant la r&eacute;f&eacute;rence <b>XX00002</b> a &eacute;t&eacute; ajout&eacute;e avec succ&egrave;s, Cliquez sur le boutton &laquo; <b>Consulter</b> &raquo; pour visualiser l&rsquo;annonce</td>
-                    </tr>                    
-                </table>                
-            </div>            
+            <table>
+                <tr>
+                    <td width="200" valign="top"><img src="/car/web/images/box.png"/></td>
+                    <td width="20"></td>
+                    <td valign="top">
+                        <div style="padding-left: 10px;font-family: TAHOMA;font-size: 10pt">
+                            <table>
+                                <tr>
+                                    <td width="10" valign="top" style="padding-top: 5px;"><img src="/car/web/images/red_fleche.png"/></td>
+                                    <td width="3"></td>
+                                    <td valign="top">Votre annonce portant la r&eacute;f&eacute;rence <b>XX00002</b> a &eacute;t&eacute; ajout&eacute;e avec succ&egrave;s, Cliquez sur le boutton &laquo; <b>Consulter</b> &raquo; pour visualiser l&rsquo;annonce<br/><br/></td>
+                                </tr>                                
+                                <tr>
+                                    <td width="10" valign="top" style="padding-top: 5px;"><img src="/car/web/images/red_fleche.png"/></td>
+                                    <td width="3"></td>
+                                    <td valign="top">Vous pouvez à tout moment modifier votre annonce en cliquant sur le boutton &laquo; <b>Editer</b> &raquo;<br/><br/></td>
+                                </tr>
+                                <tr>                                    
+                                    <td valign="top" colspan="3">
+                                        <table>
+                                            <tr>
+                                                <td><img src="/car/web/images/consulter_ann.png"/></td>
+                                                <td><img src="/car/web/images/editer_ann.png"/></td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </td>
+                </tr>
+            </table>
         </td>
     </tr>
 </table>
+<br/><br/>
+<center><span style="font-family: TAHOMA;color: #C61B00;font-size: 12pt;padding-left: 10px;font-weight: bold">Merci d'avoir choisi PauseAuto pour d&eacute;poser votre annonce. </span></center>
