0036fc45ee8b449fa62046749accb0503169a6c9
reda.ze <reda.ze@a1dfe79e-b4ea-11de-a210-8fd6ca7157a7>
2011-04-19 14:21:58 +0000

    MAJ

--- car/apps/auto/templates/layout.php
+++ car/apps/auto/templates/layout.php
@@ -103,7 +103,19 @@ dojo.addOnLoad(
 <!-- FIN DOJO --> 
 <?php include_stylesheets() ?>
 <?php include_javascripts() ?>
+<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-22831107-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body>
 <!-- <img src="/car/web/images/bg/red-christmas-lights.jpg" alt="" id="background" />  -->
